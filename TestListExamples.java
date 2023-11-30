c558d19260ca544c91ce0eca803aabf33309eac8
Beijie Cheng <bjcheng1218@gmail.com>
2023-11-29 17:22:44 -0800

    Week9

--- TestListExamples.java
+++ TestListExamples.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.*;
 
 class IsMoon implements StringChecker {
   public boolean checkString(String s) {
@@ -18,4 +19,12 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+  // @Test(timeout = 500)
+  // public void testMergeLeftEnd() {
+  //   List<String> left = Arrays.asList("a", "b", "c");
+  //   List<String> right = Arrays.asList("a", "d");
+  //   List<String> merged = ListExamples.merge(left, right);
+  //   List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
+  //   assertEquals(expected, merged);
+  // }
 }
--- grade.sh
+++ grade.sh
@@ -1,4 +1,7 @@
+#set -e
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+TPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore'
+GRADE='./grading-area/'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -8,9 +11,66 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
-
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+# https://github.com/ucsd-cse15l-w23/lab3.git
+
+files=`find student-submission`
+check=false
+
+# Find ListExample.java
+for file in $files
+do 
+    if [[ $file == "student-submission/ListExamples.java" ]]
+    then
+        # echo $file
+        echo "ListExamples.java found"
+        filename=$file
+        check=true
+    fi
+done
+
+# If the file is not found, exit
+if [[ $check == false ]]
+then
+    echo $check
+    exit 1
+else
+    echo "Found"
+fi
+
+# copy necessary files into grading-area
+cp $filename $GRADE
+cp ./TestListExamples.java $GRADE
+cp -r ./lib $GRADE
+
+# compile the java file
+javac -cp $CPATH $GRADE*.java 2>./grading-area/compile.txt
+
+cd grading-area
+java -cp $TPATH TestListExamples >test.txt
+
+# find the line of test result in test.txt
+line=`grep "Tests run:" ./test.txt`
+
+# compute the score
+if [[ -z "$line" ]]
+then
+    SCORE=100;
+else
+    TOTAL=${line:11:1}
+    FAILED=${line:25:1}
+    PASSED=$(($TOTAL-$FAILED))
+    SCORE=$(($(($PASSED))*100/$TOTAL))
+fi
+
+# print the test result
+echo "
+Total tests: $TOTAL:
+Tests Failed: $FAILED;
+Test Passed: $PASSED;
+Your Score is: $SCORE"
